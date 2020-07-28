b7c274c2d6668f0daad1247b69d7abc93e022ea5
Lestár Patrik <lrpatrik55@gmail.com>
2020-07-28 14:37:31 +0200

    Products update + colors

--- js/app.js
+++ js/app.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
   $('.products_button').click(function(){
     $("html, body").animate({scrollTop: $("#products").offset().top}, "slow");
   });
+//OPEN PRODUCT:
+  $(".product").click(function () {
+    window.open($(this).attr("href"));
+  });
+//BACK TO MENU:
+  $('.backtomenu').click(function(){
+    window.open($(this).attr("href"));
+  });
 });
